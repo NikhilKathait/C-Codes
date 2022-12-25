815b182996e3042c9c514a0c731b0ab27da25cc3
nkathait2 <nkathait2@IN-PF1ZH4RV>
2022-12-25 16:20:46 +0530

    Add project files.

---
+++ ForLoop/ForLoop.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        for(int i=0; i<10; i++)
+        {
+            System.Console.WriteLine(i);
+        }
+        System.Console.ReadLine();
+    }
+       
+}
